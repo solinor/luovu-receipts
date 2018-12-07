a1d59796cbe5059a3c0e8b7fb199629dbf031fcd
Tommi Kärkäs <tommi.karkas@gofore.com>
2018-12-07 13:13:15 +0200

    Google authentication switched to Azure AD
    Created a separate gatekeeper page for login process.
    This can be probably taken out if no intention of having multiple
    authentication back-ends exists.

--- receipt_checking/settings.py
+++ receipt_checking/settings.py
@@ -39,35 +39,24 @@ USER_EMAIL_MAP = json.loads(os.environ.get("USER_EMAIL_MAP", "{}"))
 SLACK_BOT_ACCESS_TOKEN = os.environ.get("SLACK_BOT_ACCESS_TOKEN")
 SLACK_ADMIN_EMAIL = os.environ.get("SLACK_ADMIN_EMAIL")
 
-AUTHENTICATION_BACKENDS = (
-    'googleauth.backends.GoogleAuthBackend',
-)
+AUTHENTICATION_BACKENDS = [
+    'social_core.backends.azuread_tenant.AzureADTenantOAuth2'
+]
 
-# client ID from the Google Developer Console
-GOOGLEAUTH_CLIENT_ID = os.environ.get("GOOGLEAUTH_CLIENT_ID")
+SOCIAL_AUTH_POSTGRES_JSONFIELD = os.environ.get("SOCIAL_AUTH_POSTGRES_JSONFIELD")
 
-# client secret from the Google Developer Console
-GOOGLEAUTH_CLIENT_SECRET = os.environ.get("GOOGLEAUTH_CLIENT_SECRET")
+# Azure AD Oauth keys
 
-# your app's domain, used to construct callback URLs
-GOOGLEAUTH_CALLBACK_DOMAIN = os.environ.get("GOOGLEAUTH_CALLBACK_DOMAIN")
+SOCIAL_AUTH_AZUREAD_TENANT_OAUTH2_KEY = os.environ.get("SOCIAL_AUTH_AZUREAD_TENANT_OAUTH2_KEY")
+SOCIAL_AUTH_AZUREAD_TENANT_OAUTH2_SECRET = os.environ.get("SOCIAL_AUTH_AZUREAD_TENANT_OAUTH2_SECRET")
+SOCIAL_AUTH_AZUREAD_TENANT_OAUTH2_TENANT_ID = os.environ.get("SOCIAL_AUTH_AZUREAD_TENANT_OAUTH2_TENANT_ID")
 
-# callback URL uses HTTPS (your side, not Google), default True
-GOOGLEAUTH_USE_HTTPS = os.environ.get("GOOGLEAUTH_USE_HTTPS", True) in (True, "True", "true")
-
-# restrict to the given Google Apps domain, default None
-GOOGLEAUTH_APPS_DOMAIN = os.environ.get("GOOGLEAUTH_APPS_DOMAIN")
-
-# get user's name, default True (extra HTTP request)
-GOOGLEAUTH_GET_PROFILE = True
-
-# sets value of user.is_staff for new users, default False
-GOOGLEAUTH_IS_STAFF = False
-
-# list of default group names to assign to new users
-GOOGLEAUTH_GROUPS = []
+SOCIAL_AUTH_AZUREAD_TENANT_OAUTH2_WHITELISTED_DOMAINS = os.environ.get("WHITELISTED_DOMAINS")
 
 SECURE_HSTS_SECONDS = int(os.environ.get("SECURE_HSTS_SECONDS", 0))
+
+SOCIAL_AUTH_URL_NAMESPACE = 'social'
+
 
 INSTALLED_APPS = [
     "raven.contrib.django.raven_compat",
@@ -82,7 +71,7 @@ INSTALLED_APPS = [
     # greater consistency between gunicorn and `./manage.py runserver`. See:
     # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
     # 'whitenoise.runserver_nostatic',
-    'googleauth',
+    'social_django',
     'django.contrib.staticfiles',
     'receipts',
     'compressor',
@@ -119,6 +108,52 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'receipts.middleware.NoCacheHeaders'
 ]
+
+SOCIAL_AUTH_PIPELINE = (
+    # Get the information we can about the user and return it in a simple
+    # format to create the user instance later. On some cases the details are
+    # already part of the auth response from the provider, but sometimes this
+    # could hit a provider API.
+    'social_core.pipeline.social_auth.social_details',
+
+    # Get the social uid from whichever service we're authing thru. The uid is
+    # the unique identifier of the given user in the provider.
+    'social_core.pipeline.social_auth.social_uid',
+
+    # Verifies that the current auth process is valid within the current
+    # project, this is where emails and domains whitelists are applied (if
+    # defined).
+    'social_core.pipeline.social_auth.auth_allowed',
+
+    # Checks if the current social-account is already associated in the site.
+    'social_core.pipeline.social_auth.social_user',
+
+    # Make up a username for this person, appends a random string at the end if
+    # there's any collision.
+    'social_core.pipeline.user.get_username',
+
+    # Send a validation email to the user to verify its email address.
+    # Disabled by default.
+    # 'social_core.pipeline.mail.mail_validation',
+
+    # Associates the current social details with another user account with
+    # a similar email address. Disabled by default.
+    # 'social_core.pipeline.social_auth.associate_by_email',
+
+    # Create a user account if we haven't found one yet.
+    'social_core.pipeline.user.create_user',
+
+    # Create the record that associates the social account with the user.
+    'social_core.pipeline.social_auth.associate_user',
+
+    # Populate the extra_data field in the social record with the values
+    # specified by settings (and the default ones like access_token, etc).
+    'social_core.pipeline.social_auth.load_extra_data',
+
+    # Update the user record with any changed info from the auth service.
+    'social_core.pipeline.user.user_details',
+)
+
 
 
 ROOT_URLCONF = 'receipt_checking.urls'
